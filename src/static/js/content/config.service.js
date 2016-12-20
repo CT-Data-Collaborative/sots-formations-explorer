cfec5732bb8c8e968154d17a1447715a2ca8288e
Sasha Cuerda <sasha@mapconomy.com>
2016-12-20 13:31:19 -0500

    updated to dec 2016

--- src/static/js/content/config.service.js
+++ src/static/js/content/config.service.js
@@ -2,7 +2,7 @@ angular.module('app')
     .service('appConfig', ['$q', '$http', 'lodash', function($q, $http, lodash) {
         settings = {
             'startData': '1980-01-01',
-            'endData': '2016-09-30',
+            'endData': '2016-11-31',
             'structureOrder': [
                 "All Business Entities",
                 "LLC (CT)",
