8a91462e8040cb89e63e477613305ef6d8616e9d
ilyankou <ilyankou@gmail.com>
2020-08-13 09:28:24 +0100

    Add new registration Tableau dashboard to main page

--- src/static/js/content/config.service.js
+++ src/static/js/content/config.service.js
@@ -2,7 +2,7 @@ angular.module('app')
     .service('appConfig', ['$q', '$http', 'lodash', function($q, $http, lodash) {
         settings = {
             'startData': '1980-01-01',
-            'endData': '2019-12-31',
+            'endData': '2020-07-31',
             'structureOrder': [
                 "All Business Entities",
                 "LLC (CT)",
